2fbb13d67ca8b806fcb40d27e6dbc0000dcc6236
Muawaz Saleem <muawazsaleem@gmail.com>
2024-08-08 18:48:08 +0500

    Remove Task1+Task2+Task3

--- Task1/palindrom.py
+++
@@ -1,8 +0,0 @@
-str_ = "maham"
-
-rev_ = str_[::-1]
-
-if(str_ == rev_):
-    print("True")
-else:
-    print("False")
--- Task1/practice/Even_Numbers_1-10.py
+++
@@ -1,9 +0,0 @@
-# Question 1: 
-# Write a Python program that prints all the even numbers between 1 and 10 
-# using a for loop
-
-index = 10
-
-for i in range(index+1):
-    if(i % 2 == 0):
-        print(i)
