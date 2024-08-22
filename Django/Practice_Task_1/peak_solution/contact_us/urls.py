b083626a88a8b28cbd384aa86003a751a9a3e498
Muawaz Saleem <muawazsaleem@gmail.com>
2024-08-22 20:15:04 +0500

    Getting users data on display

--- Django/Practice_Task_1/peak_solution/contact_us/urls.py
+++ Django/Practice_Task_1/peak_solution/contact_us/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.members, name='members'),
+    path('', views.user_view, name='user_view'),
+    path('details/<int:id>/', views.user_details, name='user_details')
 ]
--- Django/Practice_Task_1/peak_solution/contact_us/views.py
+++ Django/Practice_Task_1/peak_solution/contact_us/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Users
 
-def members(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+def user_view(request):
+    user_view_list = Users.objects.all().values()
+    template = loader.get_template('all_users.html')
+    context = {
+        'user_list': user_view_list,
+    }
+
+    return HttpResponse(template.render(context, request))
+
+def user_details(request, id):
+    user_detail_list = Users.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'detail_list': user_detail_list,
+    }
+
+    return HttpResponse(template.render(context, request))
