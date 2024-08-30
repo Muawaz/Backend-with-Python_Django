642b34b012fdac0191ff29714a6e8c12b07abc1a
Muawaz Saleem <muawazsaleem@gmail.com>
2024-08-30 19:52:15 +0500

    Creating model for input data

--- Django/4. Login__Signup/auth_project/auth_app/models.py
+++ Django/4. Login__Signup/auth_project/auth_app/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-
+class app_model(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
