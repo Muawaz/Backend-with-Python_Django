0d7048dc441d3589edcb4835d3155fcf3f2ebb59
Muawaz Saleem <muawazsaleem@gmail.com>
2024-08-30 19:49:33 +0500

    Switching between the three pages

--- Django/4. Login__Signup/auth_project/auth_app/urls.py
+++ Django/4. Login__Signup/auth_project/auth_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sign_up_view, name='sign_up_view')
+    path('', views.home_view, name='home_view'),
+    path('log_in/', views.log_in_view, name='log_in_view'),
+    path('sign_up/', views.sign_up_view, name='sign_up_view')
 ]
--- Django/4. Login__Signup/auth_project/auth_app/views.py
+++ Django/4. Login__Signup/auth_project/auth_app/views.py
@@ -3,5 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def home_view(request):
+    return render(request, 'home.html')
+
+def log_in_view(request):
+    return render(request, 'log_in.html')
+
 def sign_up_view(request):
     return render(request, 'sign_up.html')
