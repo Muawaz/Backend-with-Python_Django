c3a8df0ee58f2caec42655a10cfd2b1ef7388418
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-06 08:08:33 -0700

    Creating course model

--- Django/5. Online learning Platform/learning_project/learning_app/models.py
+++ Django/5. Online learning Platform/learning_project/learning_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class course_table(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    instructor = models.CharField(max_length=255)
+    duration = models.CharField(max_length=255)
