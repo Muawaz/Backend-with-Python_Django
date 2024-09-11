dd2665ccb584def748dbfe6eebd06f6b1fd671cf
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-11 10:57:22 +0500

    join queries

--- Django/5. Online learning Platform/learning_project/learning_app/models.py
+++ Django/5. Online learning Platform/learning_project/learning_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class course_table(models.Model):
-    course_id = models.IntegerField(primary_key=True)
+    course_id = models.IntegerField(primary_key=True,default="101")
     title = models.CharField(max_length=255)
     description = models.TextField()
     instructor = models.CharField(max_length=255)
