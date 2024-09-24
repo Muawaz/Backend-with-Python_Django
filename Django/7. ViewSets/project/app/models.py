6a12a208f08a05bfbe024236002ad33a6e822ae7
Muawaz Saleem <muawazsaleem@gmail.com>
2024-09-24 20:40:59 +0500

    model migrations

--- Django/7. ViewSets/project/app/models.py
+++ Django/7. ViewSets/project/app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    rollNo = models.IntegerField(primary_key=True,default="101")
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
